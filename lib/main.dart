6a0f0b1dac2d86645ba3536d18159be2aa3ab04d
smil-thakur <75114432+smil-thakur@users.noreply.github.com>
2022-09-17 22:07:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'News',
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(brightness: Brightness.dark),
       theme: ThemeData(
